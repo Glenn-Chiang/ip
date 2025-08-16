bfd79d78739deec8b76f4040919e5aeef60f6c40
Glenn <glennchiang24@gmail.com>
2025-08-16 23:15:03 +0800

    Implement echo

--- src/main/java/Glendon.java
+++ src/main/java/Glendon.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Glendon {
     private static String name = "Glendon";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm " + Glendon.name);
         System.out.println("What can I do for you?");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
