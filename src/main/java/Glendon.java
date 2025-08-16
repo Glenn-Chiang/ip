c673c075673974c05bdf0e2773c68c5ad90ec506
Glenn <glennchiang24@gmail.com>
2025-08-16 23:00:09 +0800

    Rename to Glendon

---
+++ src/main/java/Glendon.java
@@ -0,0 +1,9 @@
+public class Glendon {
+    private static String name = "Glendon";
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm " + Glendon.name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
