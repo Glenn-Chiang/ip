ba65294458542a4f61e136527279fa142114fe5a
Glenn <glennchiang24@gmail.com>
2025-09-07 13:23:40 +0800

    Use stream for TaskList find method

--- src/main/java/glendon/TaskList.java
+++ src/main/java/glendon/TaskList.java
@@ -70,12 +70,6 @@ public class TaskList {
      * @return The list of tasks whose description contains the given keyword.
      */
     public List<Task> findTask(String keyword) {
-        List<Task> results = new ArrayList<>();
-        for (Task task : this.tasks) {
-            if (task.getDescription().contains(keyword)) {
-                results.add(task);
-            }
-        }
-        return results;
+        return this.tasks.stream().filter(task -> task.getDescription().contains(keyword)).toList();
     }
 }
